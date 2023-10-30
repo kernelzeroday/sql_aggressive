4745644392a8b4512c44d0abdfa2dc7a7ab56fea
kelsey <enktal@gmail.com>
2023-10-30 13:42:16 -0700

    working

--- sqla.py
+++ sqla.py
@@ -5,6 +5,8 @@ import signal
 import sys
 import threading
 import queue
+import tempfile
+import os
 
 # Constants
 NUM_THREADS = 25
@@ -18,24 +20,40 @@ def signal_handler(sig, frame):
     sys.exit(0)
 
 def execute_batch(command, batch, command_args, output_file):
-    """ Execute the command on a batch of URLs and save the output. """
-    for url in batch:
-        full_command = f"{command} {url} {command_args}"
-        try:
-            result = subprocess.run(
-                full_command,
-                shell=True,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.STDOUT
-            )
-            with open(output_file, "a") as f:
-                f.write(f"Command: {full_command}\n")
-                f.write(result.stdout.decode())
-                f.write("\n")
-        except Exception as e:
-            with open(output_file, "a") as f:
-                f.write(f"Error executing command: {full_command}\n")
-                f.write(f"Error: {str(e)}\n")
+    """ Execute the command on a batch of URLs, save and display the output. """
+    # Create a temporary file to hold the batch of URLs
+    with tempfile.NamedTemporaryFile(mode='w+', delete=False) as tmp_file:
+        tmp_file_name = tmp_file.name
+        for url in batch:
+            tmp_file.write(url + '\n')
+
+    full_command = f"{command} {tmp_file_name} {command_args}"
+    try:
+        print(f"Executing command: {full_command}")
+        result = subprocess.run(
+            full_command,
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT
+        )
+        output = result.stdout.decode()
+        
+        # Print to terminal
+        print(output)
+        
+        # Write to output file
+        with open(output_file, "a") as f:
+            f.write(f"Command: {full_command}\n")
+            f.write(output)
+            f.write("\n")
+    except Exception as e:
+        error_msg = f"Error executing command: {full_command}\nError: {str(e)}\n"
+        print(error_msg)
+        with open(output_file, "a") as f:
+            f.write(error_msg)
+    finally:
+        # Remove the temporary file
+        os.remove(tmp_file_name)
 
 def worker(command, command_args, output_file):
     """ Worker thread function to process batches of URLs. """
@@ -47,25 +65,25 @@ def worker(command, command_args, output_file):
         url_queue.task_done()
 
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print("Usage: ./script_name.py command -o output_file.txt -i input_arguments_file.txt")
+    if len(sys.argv) < 5:
+        print("Usage: ./script_name.py 'command' -o output_file.txt -i input_arguments_file.txt")
         sys.exit(1)
 
     command = sys.argv[1]
-    output_file = sys.argv[2]
-    input_args_file = sys.argv[4]
+    output_file_flag = sys.argv[2]
+    output_file = sys.argv[3]
+    input_args_flag = sys.argv[4]
+    input_args_file = sys.argv[5]
 
     # Ensure output option is specified correctly
-    if output_file[:2] != "-o":
+    if output_file_flag != "-o":
         print("Error: Output file must be specified with -o option")
         sys.exit(1)
-    output_file = output_file[3:]  # Remove the '-o' part
 
     # Ensure input arguments file option is specified correctly
-    if input_args_file[:2] != "-i":
+    if input_args_flag != "-i":
         print("Error: Input arguments file must be specified with -i option")
         sys.exit(1)
-    input_args_file = input_args_file[3:]  # Remove the '-i' part
 
     # Read command arguments from the specified file
     with open(input_args_file, 'r') as f:
@@ -101,6 +119,4 @@ if __name__ == "__main__":
         url_queue.put(None)
     for t in threads:
         t.join()
-
-    print("Processing complete.")
 
